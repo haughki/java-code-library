eb0171ffef9b46cd1699f46c55158378ae674469
Hawkeye Parker <parker.hawkeye@gmail.com>
2015-03-02 21:13:43 -0800

    Adding CharsAndStrings and FluentAndLambdas

---
+++ src/main/java/org/haughki/codeLibrary/CharsAndStrings.java
@@ -0,0 +1,10 @@
+package org.haughki.codeLibrary;
+
+public class CharsAndStrings {
+    public static void main(String[] args) {
+        // the int value of a char is the Unicode code point:
+        char c = '不';
+        System.out.println((int) (c));
+        System.out.println((char) (19981));
+    }
+}
